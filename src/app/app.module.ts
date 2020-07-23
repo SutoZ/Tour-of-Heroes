e9d6826a5ea1e75a7afd48c1f7ca1bc5e871cd86
Zoltan Suto <suto.zoli92@gmail.com>
2020-07-23 15:29:52 +0200

    Revert "message component added"
    This reverts commit 00a5d2ef0fb703a7276bc1c40349b102f487e9c4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    MessagesComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
